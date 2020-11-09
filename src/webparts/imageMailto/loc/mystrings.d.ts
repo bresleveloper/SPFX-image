2a586722ee96468fae6071d00fac26389aaedc54
admin <ariel.rubi@gmail.com>
2020-11-09 06:56:01 +0200

    first commit

---
+++ src/webparts/imageMailto/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IImageMailtoWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ImageMailtoWebPartStrings' {
+  const strings: IImageMailtoWebPartStrings;
+  export = strings;
+}
